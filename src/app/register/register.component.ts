14fcd24dc2b5997207c491b31b1a6c1ce36966d3
Walid Bouhdif <wbouhdif@hotmail.com>
2022-12-14 04:21:20 +0100

    feature: now checks if fields are all filled in, and creates account if so

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import {Account} from "../shared/account.model";
+import { AccountType } from "../shared/account-type.model";
+import {HttpService} from "../services/http.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  valueCheckBox = "";
+  map = new Map();
+
+
+
+
+
+
+
+  registerAccount(firstName: string, lastName: string, email: string, password: string, confirmPassword: string) {
+    if (this.fieldsAreFilled(firstName, lastName, email, password, confirmPassword) && this.passwordsAreEqual(password, confirmPassword) && this.accountTypeIsSelected()) {
+      let accountType: AccountType = new AccountType(this.map.get(this.valueCheckBox), this.valueCheckBox);
+      let account: Account = new Account(null, email, password, firstName, lastName, false, accountType);
+
+
+
+    }
+  }
+
+
+  passwordsAreEqual(password: string, confirmPassword: string) {
+    return password === confirmPassword;
+  }
+
+  accountTypeIsSelected() {
+    return this.valueCheckBox !== "";
+  }
+
+
+  fieldsAreFilled(firstName: string, lastName: string, email: string, password: string, confirmPassword: string) {
+    return firstName !== "" && lastName !== "" && email !== "" && password !== "" && confirmPassword !== "";
+  }
+
+
+  ngOnInit(): void {
+    this.map.set("spine", "d2de260f-097e-436f-85df-02419a41257a");
+    this.map.set("caregiver", "a0211c55-911f-4520-b240-da9f670eb976");
+  }
+
+}
