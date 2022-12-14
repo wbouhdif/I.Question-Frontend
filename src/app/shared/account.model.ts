0407abd76777324939d3b894ba6953115acb5205
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-12-14 16:09:34 +0100

    Adds getters and setters to account.model and adds undefined as possible field type.

--- src/app/shared/account.model.ts
+++ src/app/shared/account.model.ts
@@ -1,21 +1,78 @@
 import { AccountType } from "./account-type.model";
 
 export class Account {
-    private id: string | null;
-    private email: string | null;
-    private password: string | null;
-    private firstName: string | null;
-    private lastName: string | null;
-    private authorised: boolean | null;
-    private type: AccountType | null;
+    private _id: string | undefined;
+    private _email: string | undefined;
+    private _password: string | undefined;
+    private _firstName: string | undefined;
+    private _lastName: string | undefined;
+    private _authorised: boolean | undefined;
+    private _type: AccountType | undefined;
 
-    constructor(id? : string | null, email?: string, password?: string, firstName?: string, lastName?: string, authorised?: boolean, type?: AccountType) {
-        this.id = id ?? null;
-        this.email = email ?? null;
-        this.password = password ?? null;
-        this.firstName = firstName ?? null;
-        this.lastName = lastName ?? null;
-        this.authorised = authorised ?? null;
-        this.type = type ?? null;
+    constructor(id? : string | undefined, email?: string, password?: string, firstName?: string, lastName?: string, authorised?: boolean, type?: AccountType) {
+        this._id = id ?? undefined;
+        this._email = email ?? undefined;
+        this._password = password ?? undefined;
+        this._firstName = firstName ?? undefined;
+        this._lastName = lastName ?? undefined;
+        this._authorised = authorised ?? undefined;
+        this._type = type ?? undefined;
     }
+
+
+  get id(): string | undefined {
+    return this._id;
+  }
+
+  set id(value: string | undefined) {
+    this._id = value;
+  }
+
+  get email(): string | undefined {
+    return this._email;
+  }
+
+  set email(value: string | undefined) {
+    this._email = value;
+  }
+
+  get password(): string | undefined {
+    return this._password;
+  }
+
+  set password(value: string | undefined) {
+    this._password = value;
+  }
+
+  get firstName(): string | undefined {
+    return this._firstName;
+  }
+
+  set firstName(value: string | undefined) {
+    this._firstName = value;
+  }
+
+  get lastName(): string | undefined {
+    return this._lastName;
+  }
+
+  set lastName(value: string | undefined) {
+    this._lastName = value;
+  }
+
+  get authorised(): boolean | undefined {
+    return this._authorised;
+  }
+
+  set authorised(value: boolean | undefined) {
+    this._authorised = value;
+  }
+
+  get type(): AccountType | undefined {
+    return this._type;
+  }
+
+  set type(value: AccountType | undefined) {
+    this._type = value;
+  }
 }
