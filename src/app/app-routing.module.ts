c5f95b86244d3b7739556c4dada4195c1c5d905a
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-12-16 15:45:31 +0100

    Adds route for QuestionnairesComponent to app-routing.module.ts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import {HomeComponent} from "./home/home.component";
+import {QuestionnairesComponent} from "./questionnaires/questionnaires.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'questionnaires', component: QuestionnairesComponent}
 
 ];
 
