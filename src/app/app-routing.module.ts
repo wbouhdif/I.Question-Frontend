de9bb12b59664a3a118038ddbafc0033818cc848
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-12-19 11:02:18 +0100

    Adds AnsweredQuestionnaireComponent as route to app-routing.module.ts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import {HomeComponent} from "./home/home.component";
 import {QuestionnairesComponent} from "./questionnaires/questionnaires.component";
+import {AnsweredQuestionnairesComponent} from "./answered-questionnaires/answered-questionnaires.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'questionnaires', component: QuestionnairesComponent}
-
+  { path: 'questionnaires', component: QuestionnairesComponent },
+  { path: 'answered-questionnaires', component: AnsweredQuestionnairesComponent }
 ];
 
 @NgModule({
