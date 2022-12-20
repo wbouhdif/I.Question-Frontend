a9e791509ea7702594f8a6506e90873811c864f7
Walid Bouhdif <wbouhdif@hotmail.com>
2022-12-20 18:34:19 +0100

    accounts now load in dynamically into the view

---
+++ cypress.config.ts
@@ -0,0 +1,8 @@
+// noinpection TypeScriptValidateTypes
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:4200",
+  },
+});
---
+++ src/app/authorise-accounts/authorise-accounts.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import {Account} from "../shared/account.model";
+import {HttpService} from "../services/http.service";
+import {AccountType} from "../shared/account-type.model";
+
+@Component({
+  selector: 'app-authorise-accounts',
+  templateUrl: './authorise-accounts.component.html',
+  styleUrls: ['./authorise-accounts.component.scss']
+})
+export class AuthoriseAccountsComponent {
+
+  accounts: Account[] = [];
+
+  selectedAccount: any
+
+  constructor(private httpService: HttpService) {}
+
+  assignAccounts() {
+    this.httpService.get('account').subscribe({
+      next: (response) => {
+        for (let account of response.body) {
+          if (account.type?.name !== 'Admin'){
+            this.accounts.push(account)
+          }
+        }
+      },
+      error: (error) => { console.log(error) }
+      });
+  }
+
+
+
+  ngOnInit() {
+    this.assignAccounts()
+  }
+
+  editAccount(account: Account) {
+
+
+  }
+
+  deleteAccount(account: Account) {
+
+  }
+}
