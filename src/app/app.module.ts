b99eea2e598c4e6d2234c9fc4a07b130645ee2d3
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-02 15:34:01 +0100

    Adds new components to app.module.ts and imports MatIconModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,11 @@ import { HomeComponent } from "./home/home.component";
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuestionnairesComponent } from './questionnaires/questionnaires.component';
 import { AnsweredQuestionnairesComponent } from './answered-questionnaires/answered-questionnaires.component';
+import { EditQuestionnaireComponent } from './edit-questionnaire/edit-questionnaire.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableComponent } from './table/table.component';
+
 
 @NgModule({
   declarations: [
@@ -20,13 +26,18 @@ import { AnsweredQuestionnairesComponent } from './answered-questionnaires/answe
     HomeComponent,
     NavbarComponent,
     QuestionnairesComponent,
-    AnsweredQuestionnairesComponent
+    AnsweredQuestionnairesComponent,
+    EditQuestionnaireComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
