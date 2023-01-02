042cfa3fa045a6e4a40ece8ca4bf56d125a985a8
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-02 15:08:13 +0100

    Updates user.service to utilize localStorage and add logOut() method.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Account} from "../shared/account.model";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,27 @@ export class UserService {
 
   private activeAccount: Account | undefined;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  setActiveAccount(activeAccount: Account | undefined) {
+  setActiveAccount(activeAccount: Account) {
     this.activeAccount = activeAccount;
+    localStorage.setItem('active-account', JSON.stringify(activeAccount));
+  }
+
+  logOut() {
+    localStorage.removeItem('active-account');
   }
 
   getActiveAccount() {
+    let activeAccount: any = localStorage.getItem('active-account');
+    if (activeAccount == undefined) {
+      this.router.navigate(['login']);
+      return;
+    }
+    activeAccount = JSON.parse(activeAccount);
+
+    this.activeAccount = activeAccount;
+
     return this.activeAccount;
   }
 }
