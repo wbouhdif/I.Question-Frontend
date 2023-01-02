80c0e26b280dd835e5a024021ecc33a98b2dc416
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-02 15:09:18 +0100

    Adds table component for reusability.

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent {
+  @Input() columns: string[] = [];
+  @Input() height: any;
+}
