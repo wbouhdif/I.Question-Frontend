e595571fdb98d015a0ce1781b8fbc2c728952d96
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-02 15:06:56 +0100

    Makes fields of all the models public instead of using getters and setter, aswell as removing unnecessary fields and adding fields.

--- src/app/shared/account-type.model.ts
+++ src/app/shared/account-type.model.ts
@@ -1,26 +1,10 @@
 export class AccountType {
-    private _id: string | undefined;
-    private _name: string | undefined;
+
+    private id: string | undefined;
+    private name: string | undefined;
 
     constructor(id?: string | undefined, name?: string) {
-        this._id = id;
-        this._name = name;
+        this.id = id;
+        this.name = name;
     }
-
-  get id(): string | undefined {
-    return this._id;
-  }
-
-  set id(value: string | undefined) {
-    this._id = value;
-  }
-
-  get name(): string | undefined {
-    return this._name;
-  }
-
-  set name(value: string | undefined) {
-    this._name = value;
-  }
-
 }
--- src/app/shared/employed-question.model.ts
+++ src/app/shared/employed-question.model.ts
@@ -3,16 +3,17 @@ import {Questionnaire} from "./questionnaire.model";
 
 export class EmployedQuestion {
 
-  private id: string | undefined;
-  private question: Question | undefined;
-  private questionnaire: Questionnaire | undefined;
-  private position: number | undefined;
+  id: string | undefined;
+  question: Question | undefined;
+  questionnaire: Questionnaire | undefined;
+  position: number | undefined;
+  mandatory: boolean | undefined
 
-
-  constructor(id?: string | undefined, question?: Question, questionnaire?: Questionnaire, position?: number) {
+  constructor(id?: string | undefined, question?: Question, questionnaire?: Questionnaire, position?: number, mandatory?: boolean) {
     this.id = id;
     this.question = question;
     this.questionnaire = questionnaire;
     this.position = position;
+    this.mandatory = mandatory
   }
 }
