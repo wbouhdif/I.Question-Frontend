c27fbbb112a8142ead0aa5825d609c27988cff17
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-12 10:57:24 +0100

    Adds account name in navbar.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from "../services/user.service";
+import {Account} from "../shared/account.model";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  accountName: any;
 
   constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.accountName = this.userService.getActiveAccount()?.firstName + " " + this.userService.getActiveAccount()?.lastName;
   }
 
   toHome() {}
