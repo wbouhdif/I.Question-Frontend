52a7c9d7db0a550bc0c4db18f66034e6c1c386c6
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-23 00:34:06 +0100

    Removes redundant private modifier and methods.

--- src/app/shared/account-type.model.ts
+++ src/app/shared/account-type.model.ts
@@ -1,18 +1,10 @@
 export class AccountType {
 
-    private id: string | undefined;
+    id: string | undefined;
     name: string | undefined;
 
     constructor(id?: string | undefined, name?: string) {
         this.id = id;
         this.name = name;
-    }
-
-    public getId(): string | undefined {
-        return this.id;
-    }
-
-    public getName(): string | undefined {
-        return this.name;
     }
 }
