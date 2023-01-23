497c8392e10348a0e7bec2fe7f1ada582c79c096
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-23 15:08:38 +0100

    Utilizes warning alert before deleting answered questionnaire.

--- src/app/answered-questionnaires/answered-questionnaires.component.ts
+++ src/app/answered-questionnaires/answered-questionnaires.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {Questionnaire} from "../shared/questionnaire.model";
 import {HttpService} from "../services/http.service";
 import {AnsweredQuestionnaire} from "../shared/answered-questionnaire.model";
 import {UserService} from "../services/user.service";
+import {AlertService} from "../services/alert.service";
 
 @Component({
   selector: 'app-answered-questionnaires',
@@ -14,7 +14,7 @@ export class AnsweredQuestionnairesComponent implements OnInit {
   answeredQuestionnaires: AnsweredQuestionnaire[] = [];
   selectedQuestionnaire: any;
 
-  constructor(private httpService: HttpService, private userService: UserService) {}
+  constructor(private httpService: HttpService, private userService: UserService, private alertService: AlertService) {}
 
   ngOnInit() {
     this.assignAnsweredQuestionnaires();
@@ -36,6 +36,19 @@ export class AnsweredQuestionnairesComponent implements OnInit {
 
   setSelectedQuestionnaire(answeredQuestionnaire: AnsweredQuestionnaire) {
     this.selectedQuestionnaire === answeredQuestionnaire ? this.selectedQuestionnaire = undefined : this.selectedQuestionnaire = answeredQuestionnaire;
+  }
+
+  showDeleteAlert() {
+    this.alertService.fireWarning(
+      'Je staat op het punt om de vragenlijst: '
+      + '"' + this.selectedQuestionnaire.questionnaire.name + '", ingevuld door: '
+      + '"' + this.selectedQuestionnaire.clientName + '"'
+      + ' te verwijderen. Je kan dit niet ongedaan maken!',)
+      .then((result) => {
+      if(result.isConfirmed){
+        this.delete()
+      }
+    })
   }
 
 }
