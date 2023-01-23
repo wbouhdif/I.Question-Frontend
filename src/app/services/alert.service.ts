3ebd4be2fe4cde851f1603669a7d072a141b2d71
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-23 15:07:27 +0100

    Adds alert.service.ts with method fireWarning().

---
+++ src/app/services/alert.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import swal from 'sweetalert2'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+
+  fireWarning(text: string) {
+    return swal.fire({
+      title: 'Weet je het zeker?',
+      text: text,
+      icon: 'warning',
+      iconColor: '#e95c4b',
+      showCancelButton: true,
+      confirmButtonColor: '#e95c4b',
+      cancelButtonColor: '#1d2951',
+      confirmButtonText: 'Ja, verwijder het',
+      cancelButtonText: 'Nee, annuleer het',
+    })
+  }
+}
