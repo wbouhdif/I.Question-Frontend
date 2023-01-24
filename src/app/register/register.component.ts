4b22eed96514e36328293dc60f589d66eceda8bf
Walid Bouhdif <wbouhdif@hotmail.com>
2023-01-24 21:52:09 +0100

    generated new "password-strength" component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import {Account} from "../shared/account.model";
-import { AccountType } from "../shared/account-type.model";
-import {HttpService} from "../services/http.service";
+import { HttpService} from "../services/http.service";
+import { FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -10,48 +9,58 @@ import {HttpService} from "../services/http.service";
 })
 export class RegisterComponent {
 
-  valueCheckBox = "";
-  map = new Map();
+  registerForm: FormGroup = new FormGroup({});
+
+  roles = [
+    {name: 'Zorgverlener', value: 'a0211c55-911f-4520-b240-da9f670eb976'},
+    {name: 'Spine-medewerker', value: 'd2de260f-097e-436f-85df-02419a41257a'}];
 
 
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      password: ['', Validators.required],
+      confirmPassword: [''],
+      role: ['']
+    })
+  }
+
+  /*  registerAccount(firstName: string, lastName: string, email: string, password: string, confirmPassword: string) {
+        let account: Account = new Account(undefined, email, password, firstName, lastName, false, accountType);
+
+        this.httpService.post("account/register", account).subscribe({
+          next: (response) => {
+            console.log(response);
+          },
+          error: (error) => {
+            console.log(error);
+          }
+
+        });
+      }*/
+
+  onSubmit() {
 
   }
 
-  registerAccount(firstName: string, lastName: string, email: string, password: string, confirmPassword: string) {
-    if (this.fieldsAreFilled(firstName, lastName, email, password, confirmPassword) && this.passwordsAreEqual(password, confirmPassword) && this.accountTypeIsSelected()) {
-      let accountType: AccountType = new AccountType(this.map.get(this.valueCheckBox), this.valueCheckBox);
-      let account: Account = new Account(undefined, email, password, firstName, lastName, false, accountType);
-
-      this.httpService.post("account/register", account).subscribe({
-        next: (response) => {
-          console.log(response);
-        },
-        error: (error) => {
-          console.log(error);
-        }
-
-      });
+  allFieldsFilled(): boolean {
+    for(let key in this.registerForm.value) {
+      if (this.registerForm.get(key)?.value == '') {
+        alert('Vul alle velden in');
+        return false;
+      }
     }
+    return true;
   }
 
-  passwordsAreEqual(password: string, confirmPassword: string) {
-    return password === confirmPassword;
-  }
-
-  accountTypeIsSelected() {
-    return this.valueCheckBox !== "";
-  }
-
-
-  fieldsAreFilled(firstName: string, lastName: string, email: string, password: string, confirmPassword: string) {
-    return firstName !== "" && lastName !== "" && email !== "" && password !== "" && confirmPassword !== "";
-  }
-
-
-  ngOnInit(): void {
-    this.map.set("spine", "d2de260f-097e-436f-85df-02419a41257a");
-    this.map.set("caregiver", "a0211c55-911f-4520-b240-da9f670eb976");
+  passwordsMatch(): boolean {
+    if (this.registerForm.get('password')?.value != this.registerForm.get('confirmPassword')?.value) {
+      alert('Wachtwoorden komen niet overeen');
+      return false;
+    }
+    return true;
   }
 
 }
