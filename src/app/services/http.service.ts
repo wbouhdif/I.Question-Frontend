3c578982596bdd2ecf5cf860487424925cbf2a40
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-24 18:12:40 +0100

    Adds jwtToken to requestOptions.

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { jsonIgnoreReplacer} from "json-ignore";
+import {UserService} from "./user.service";
 
 @Injectable({
   providedIn: 'root'
@@ -8,30 +9,42 @@ import { jsonIgnoreReplacer} from "json-ignore";
 export class HttpService {
 
   private apiPath: string = 'http://localhost:8080/api/';
-  private requestOptions: Object = {
-    observe: 'response',
-    responseType: 'json',
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
-  };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userService: UserService) { }
 
   get(destination: string) {
-    return this.http.get<any>(this.apiPath + destination, this.requestOptions)
+    return this.http.get<any>(this.apiPath + destination, this.getRequestOptions())
   }
 
   post(destination: string, body: any) {
     body = (JSON.stringify(body, jsonIgnoreReplacer));
 
-    return this.http.post<any>(this.apiPath + destination, body, this.requestOptions);
+    return this.http.post<any>(this.apiPath + destination, body, this.getRequestOptions());
   }
 
   delete(destination: string) {
-    return this.http.delete(this.apiPath + destination, this.requestOptions)
+    return this.http.delete(this.apiPath + destination, this.getRequestOptions())
   }
 
   put(destination: string, body: any) {
-    let jsonBody = JSON.parse(JSON.stringify(body));
-    return this.http.put(this.apiPath + destination, jsonBody)
+    body = (JSON.stringify(body, jsonIgnoreReplacer));
+
+    return this.http.put(this.apiPath + destination, body, this.getRequestOptions())
+  }
+
+  private getRequestOptions() {
+
+    let headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+    if (this.userService.getJwtToken() != undefined) {
+      headers = headers.append('Authorization', "Bearer " + this.userService.getJwtToken().replace("\"", ""))
+    }
+
+    let requestOptions: Object = {
+      observe: 'response',
+      responseType: 'json',
+      headers: headers
+    };
+    return requestOptions;
   }
 }
